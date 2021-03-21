80fb0cbec4ac88d81c1bb589911269a52d3f40ae
Marcos Henrique <ok-marcos@hotmail.com>
2021-03-21 12:35:31 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import Notifications from '~/components/Notifications';
+
+import logo from '~/assets/logo-purple.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Notifications />
+
+          <Profile>
+            <div>
+              <strong>{profile.name}</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src={
+                (profile.avatar && profile.avatar.url) ||
+                'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png'
+              }
+              alt="Marcos Henrique"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
